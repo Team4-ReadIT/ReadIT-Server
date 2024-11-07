77d07e3708aa605ff56f8521d25e3cbb090b09bb
khee2 <21keunghee@gmail.com>
2024-11-07 23:36:54 +0900

    feat: 하이라이트 endIndex가 startIndex보다 작거나 같다는 제약조건 걸기

--- src/main/java/com/team4/readit/domain/highlight/dto/request/HighlightSaveRequestDto.java
+++ src/main/java/com/team4/readit/domain/highlight/dto/request/HighlightSaveRequestDto.java
@@ -1,10 +1,23 @@
 package com.team4.readit.domain.highlight.dto.request;
 
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record HighlightSaveRequestDto(
-        @NotNull(message = "startIndex는 필수입니다.") @Min(value = 0, message = "startIndex는 0 이상이어야 합니다.") Integer startIndex,
-        @NotNull(message = "endIndex는 필수입니다.") @Min(value = 0, message = "endIndex는 0 이상이어야 합니다.") Integer endIndex,
-    String memoText
-){}
+        @NotNull(message = "startIndex는 필수입니다.")
+        @Min(value = 0, message = "startIndex는 0 이상이어야 합니다.")
+        Integer startIndex,
+
+        @NotNull(message = "endIndex는 필수입니다.")
+        @Min(value = 0, message = "endIndex는 0 이상이어야 합니다.")
+        Integer endIndex,
+
+        String memoText
+) {
+
+    @AssertTrue(message = "endIndex는 startIndex보다 작거나 같아야 합니다.")
+    public boolean isEndIndexValid() {
+        return startIndex == null || endIndex == null || startIndex <= endIndex;
+    }
+}
