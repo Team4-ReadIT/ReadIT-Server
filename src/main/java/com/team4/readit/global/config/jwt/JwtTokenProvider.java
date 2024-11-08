87396dce332914d05ce9cde4dee43f8bdd8f01ae
khee2 <21keunghee@gmail.com>
2024-11-08 16:29:19 +0900

    feat: jar 파일 빌드

--- src/main/java/com/team4/readit/global/config/jwt/JwtTokenProvider.java
+++ src/main/java/com/team4/readit/global/config/jwt/JwtTokenProvider.java
@@ -1,40 +1,36 @@
 package com.team4.readit.global.config.jwt;
 
 import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
 import java.util.Date;
-import jakarta.annotation.PostConstruct;
 
 @Component
 @RequiredArgsConstructor
 public class JwtTokenProvider {
-    private final JwtProperties jwtProperties;
-    private Key key;
+    @Value("${jwt.secret}")
+    private String secret;
 
-    @PostConstruct
-    protected void init() {
-        key = Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes());
-    }
+    @Value("${jwt.access-token-validity-in-seconds}")
+    private long accessTokenValidityInSeconds;
 
     public String createToken(String email) {
         Claims claims = Jwts.claims().setSubject(email);
         Date now = new Date();
-        Date validity = new Date(now.getTime() + jwtProperties.getAccessTokenValidityInSeconds() * 1000);
+        Date validity = new Date(now.getTime() + accessTokenValidityInSeconds * 1000);
+
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(validity)
-                .signWith(key, SignatureAlgorithm.HS256)
+                .signWith(SignatureAlgorithm.HS256, secret)
                 .compact();
     }
-
     public String getEmailFromToken(String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(key)
+                .setSigningKey(secret)
                 .build()
                 .parseClaimsJws(token)
                 .getBody()
@@ -43,7 +39,7 @@ public class JwtTokenProvider {
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            Jwts.parserBuilder().setSigningKey(secret).build().parseClaimsJws(token);
             return true;
         } catch (JwtException | IllegalArgumentException e) {
             return false;
